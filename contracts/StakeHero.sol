054a86a0940b617d57d55f1fb942aa2c6dd0c650
Pedro Medina Faria <pedro@Pedros-MacBook-Pro.local>
2022-03-07 23:07:41 -0300

    update tests

--- contracts/StakeHero.sol
+++ contracts/StakeHero.sol
@@ -2,8 +2,22 @@
 pragma solidity ^0.8.7;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract StakeHero {
+interface IRytellHero {
+  function walletOfOwner(address owner_)
+    external
+    view
+    returns (uint256[] memory);
+
+  function safeTransferFrom(
+    address from,
+    address to,
+    uint256 tokenId
+  ) external;
+}
+
+contract StakeHero is IERC721Receiver{
   struct HeroStatus {
     bool staked;
     uint256 lastStaked;
@@ -14,10 +28,53 @@ contract StakeHero {
   mapping(address => HeroStatus[]) stakedHeros;
 
   event StakedAHero(address who, uint256 heroNumber);
+  event ReceivedERC721(
+    address operator,
+    address from,
+    uint256 tokenId,
+    bytes data
+  );
+
+  address rytellHerosContract;
+
+  constructor(address _rytellHerosContract) {
+    rytellHerosContract = _rytellHerosContract;
+  }
+
+  function onERC721Received(
+    address operator,
+    address from,
+    uint256 tokenId,
+    bytes calldata data
+  ) override external returns (bytes4) {
+    emit ReceivedERC721(operator, from, tokenId, data);
+    return IERC721Receiver(this).onERC721Received.selector;
+  }
+
+  function senderOwnsHero(uint256 heroNumber) public view returns (bool) {
+    uint256[] memory accountHeros = IRytellHero(rytellHerosContract)
+      .walletOfOwner(msg.sender);
+    for (uint256 index = 0; index < accountHeros.length; index++) {
+      if (accountHeros[index] == heroNumber) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function acquireOwnership(uint256 heroNumber) public {
+    IRytellHero(rytellHerosContract).safeTransferFrom(
+      msg.sender,
+      address(this),
+      heroNumber
+    );
+  }
 
   function stake(uint256 heroNumber) public {
+    require(senderOwnsHero(heroNumber), "Rytell: you don't own this hero");
+    acquireOwnership(heroNumber);
     HeroStatus[] storage herosOfAccount = stakedHeros[msg.sender];
-
     if (herosOfAccount.length > 0) {
       bool foundHero = false;
       for (uint256 index = 0; index < herosOfAccount.length; index++) {
@@ -29,12 +86,13 @@ contract StakeHero {
           herosOfAccount[index].lastStaked = block.timestamp;
           herosOfAccount[index].staked = true;
           foundHero = true;
+          emit StakedAHero(msg.sender, heroNumber);
           return;
         }
       }
 
       if (foundHero == false) {
-        stakedHeros[msg.sender].push(
+        herosOfAccount.push(
           HeroStatus({
             staked: true,
             lastStaked: block.timestamp,
@@ -42,9 +100,10 @@ contract StakeHero {
             heroId: heroNumber
           })
         );
+        emit StakedAHero(msg.sender, heroNumber);
       }
     } else {
-      stakedHeros[msg.sender].push(
+      herosOfAccount.push(
         HeroStatus({
           staked: true,
           lastStaked: block.timestamp,
@@ -52,6 +111,18 @@ contract StakeHero {
           heroId: heroNumber
         })
       );
+      emit StakedAHero(msg.sender, heroNumber);
     }
+  }
+
+  function unstake(uint256 heroNumber) public view {
+    require(senderOwnsHero(heroNumber), "Rytell: you don't own this hero");
+    HeroStatus[] storage herosOfAccount = stakedHeros[msg.sender];
+    require(
+      herosOfAccount.length > 0,
+      "Rytell: you don't have any staked hero"
+    );
+
+    // TODO unstake
   }
 }
--- test/index.ts
+++ test/index.ts
@@ -8,12 +8,11 @@ describe("Staking Hero", function () {
   let heros: any;
   let herosCollectionAdmin: any;
   this.beforeEach(async function () {
+    // addresses
     [herosCollectionAdmin, stakerAccount, anotherStakerAccount] =
       await ethers.getSigners();
-    const StakeHero = await ethers.getContractFactory("StakeHero");
-    stakeHero = await StakeHero.deploy();
-    await stakeHero.deployed();
 
+    // deploy heros collection and mint some
     const Heros = await ethers.getContractFactory("Rytell");
     heros = await Heros.deploy(
       "ipfs://QmXHJfoMaDiRuzgkVSMkEsMgQNAtSKr13rtw5s59QoHJAm/",
@@ -26,6 +25,23 @@ describe("Staking Hero", function () {
     await heros
       .connect(stakerAccount)
       .mint(6, { value: ethers.utils.parseEther("15") });
+
+    await heros
+      .connect(anotherStakerAccount)
+      .mint(4, { value: ethers.utils.parseEther("10") });
+
+    // deploy hero staking
+    const StakeHero = await ethers.getContractFactory("StakeHero");
+    stakeHero = await StakeHero.deploy(heros.address);
+    await stakeHero.deployed();
+
+    // Approve for all
+    await heros
+      .connect(stakerAccount)
+      .setApprovalForAll(stakeHero.address, true);
+    await heros
+      .connect(anotherStakerAccount)
+      .setApprovalForAll(stakeHero.address, true);
   });
 
   it("Should let a person stake a couple heros", async function () {
@@ -33,17 +49,26 @@ describe("Staking Hero", function () {
     expect(mintedHeros.length).to.be.greaterThan(0);
     await expect(stakeHero.connect(stakerAccount).stake(mintedHeros[0])).not.to
       .be.reverted;
-    await expect(
-      stakeHero.connect(stakerAccount).stake(mintedHeros[0])
-    ).to.be.revertedWith("Rytell: hero is already staked");
+
+    // Stake another hero
     await expect(stakeHero.connect(stakerAccount).stake(mintedHeros[1])).not.to
       .be.reverted;
   });
 
-  it("A user should not be able to stake a hero they dont own", async function () {
+  it("A user should not be able to stake a hero they don't own", async function () {
     const mintedHeros = await heros.walletOfOwner(stakerAccount.address);
     expect(mintedHeros.length).to.be.greaterThan(0);
-    await expect(stakeHero.connect(anotherStakerAccount).stake(mintedHeros[0]))
-      .to.be.reverted;
+    await expect(
+      stakeHero.connect(anotherStakerAccount).stake(mintedHeros[0])
+    ).to.be.revertedWith("Rytell: you don't own this hero");
   });
+
+  it("After staking, the contract should be the owner of the hero", async function () {
+    const mintedHeros = await heros.walletOfOwner(stakerAccount.address);
+    await stakeHero.connect(stakerAccount).stake(mintedHeros[0]);
+    const stakedHeros = await heros.walletOfOwner(stakeHero.address);
+    expect(stakedHeros[0].toString()).to.equal(mintedHeros[0].toString());
+  });
+
+  // TODO should also test events
 });
